447fb7ce290aceba2603b68704834f722bc04e03
Julian Koripamo <jkoripamo@gmail.com>
2023-11-10 21:30:11 +0100

    home screen

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,205 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:sushi_restaurant/components/button.dart';
+import 'package:sushi_restaurant/models/shop.dart';
+import 'package:sushi_restaurant/screens/food_details_screen.dart';
+import 'package:sushi_restaurant/theme/colors.dart';
+
+import '../components/food_tile.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // navigate to food detail screen
+  void navigateToFoodDetailsScreen(int index) {
+    // get the shop and it's menu
+    final shop = context.read<Shop>();
+    final foodMenu = shop.foodMenu;
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: ((context) => FoodDetailsScreen(food: foodMenu[index])),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // get the shop and it's menu
+    final shop = context.read<Shop>();
+    final foodMenu = shop.foodMenu;
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        foregroundColor: Colors.grey[800],
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: const Icon(Icons.menu),
+        title: const Center(child: Text('Tokyo')),
+        actions: [
+          // cart button
+          IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/cartscreen');
+            },
+            icon: const Icon(Icons.shopping_cart),
+          )
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // promo banner
+          Container(
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.symmetric(horizontal: 25),
+            padding: const EdgeInsets.all(25),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // promo message
+                    Text(
+                      "Get 30% Discount",
+                      style: GoogleFonts.dmSerifDisplay(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+
+                    const SizedBox(height: 20),
+
+                    // redeem promo button
+                    MyButton(
+                      title: 'Redeem',
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+
+                // image
+                Image.asset(
+                  'assets/images/salmon_many.png',
+                  height: 100,
+                )
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // search bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: const BorderSide(color: Colors.white),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                hintText: 'Search...',
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // menu list
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Text(
+              'Food Menu',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[800],
+              ),
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: foodMenu.length,
+              itemBuilder: (context, index) => FoodTile(
+                  food: foodMenu[index],
+                  onTap: () {
+                    navigateToFoodDetailsScreen(index);
+                  }),
+            ),
+          ),
+
+          const SizedBox(height: 25),
+
+          // popular food
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            margin: const EdgeInsets.only(left: 25, bottom: 25, right: 25),
+            padding: const EdgeInsets.all(20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    // image
+                    Image.asset(
+                      'assets/images/sushi.png',
+                      height: 60,
+                    ),
+
+                    const SizedBox(width: 20),
+
+                    // name and price
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Salmon Eggs',
+                          style: GoogleFonts.dmSerifDisplay(fontSize: 18),
+                        ),
+                        const SizedBox(height: 10),
+                        Text(
+                          '\$21.00',
+                          style: TextStyle(
+                            color: Colors.grey[700],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+
+                // heart
+                const Icon(
+                  Icons.favorite_outline,
+                  color: Colors.grey,
+                  size: 28,
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
