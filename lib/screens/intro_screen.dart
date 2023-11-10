117c3bcc5cc5e330bc1f380e40c91075401bba02
Julian Koripamo <jkoripamo@gmail.com>
2023-11-10 21:29:48 +0100

    intro screen

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -1,10 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sushi_restaurant/components/button.dart';
+import 'package:sushi_restaurant/theme/colors.dart';
 
 class IntroScreen extends StatelessWidget {
   const IntroScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(height: 25),
+
+            // shop name
+            Text(
+              'SUSHI SPOT',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // icon
+            Padding(
+              padding: const EdgeInsets.all(50.0),
+              child: Image.asset('assets/images/sushi.png'),
+            ),
+
+            const SizedBox(height: 25),
+
+            // title
+            Text(
+              "THE TASTE OF JAPANESE FOOD",
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 44,
+                color: Colors.white,
+              ),
+            ),
+
+            // subtitle
+            Text(
+              "Feel the taste of the most popular Japanese food from anywhere and anytime",
+              style: TextStyle(
+                color: Colors.grey[300],
+                height: 2,
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // get started button
+            MyButton(
+                title: 'Get Started',
+                onTap: () {
+                  Navigator.pushNamed(context, '/homescreen');
+                }),
+          ],
+        ),
+      ),
+    );
   }
 }
